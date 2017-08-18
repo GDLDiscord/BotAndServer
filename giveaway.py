9313978ea191a4d3f983c0c8d586db88ebc7ca33
Ewan <vulcanv6@hotmail.com>
2017-08-18 23:12:16 +0100

    Update giveaway.py
    main code

--- giveaway.py
+++ giveaway.py
@@ -1,1 +1,7 @@
 
+@commands.command(pass_context=True)
+    async def giveaway(self,em):
+    	"""Enter The Gift Card Giveaway!"""
+    	em = discord.Embed(description='YOU HAVE BEEN ENTERED INTO THE GIFTCARD GIVEAWAY!', colour=0xFF0000)
+    	em.set_author(name='GIVEAWAY BOT', url='https://noodler.co/')
+    	em.set_thumbnail(url='http://isahotel.com.au/wp-content/uploads/2016/02/Gift_Card.jpg')
