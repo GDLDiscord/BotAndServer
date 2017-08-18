9c244b2b2125030e3511dd22020eca867fe9425d
Ewan <vulcanv6@hotmail.com>
2017-08-18 23:15:30 +0100

    Update giveaway.py
    added reqs

--- giveaway.py
+++ giveaway.py
@@ -1,7 +1,17 @@
+import discord
 
-@commands.command(pass_context=True)
-    async def giveaway(self,em):
+class Giveaway:
+    """General Dev Bot Commands"""
+
+    def __init__(self, bot):
+        self.bot = bot
+        
+        @commands.command(pass_context=True)
+        async def giveaway(self,em):
     	"""Enter The Gift Card Giveaway!"""
-    	em = discord.Embed(description='YOU HAVE BEEN ENTERED INTO THE GIFTCARD GIVEAWAY!', colour=0xFF0000)
-    	em.set_author(name='GIVEAWAY BOT', url='https://noodler.co/')
-    	em.set_thumbnail(url='http://isahotel.com.au/wp-content/uploads/2016/02/Gift_Card.jpg')
+            em = discord.Embed(description='YOU HAVE BEEN ENTERED INTO THE GIFTCARD GIVEAWAY!', colour=0xFF0000)
+   	        em.set_author(name='GIVEAWAY BOT', url='discord.me/gdl')
+   	        em.set_thumbnail(url='http://isahotel.com.au/wp-content/uploads/2016/02/Gift_Card.jpg')
+
+def setup(bot):
+    bot.add_cog(General(bot))
