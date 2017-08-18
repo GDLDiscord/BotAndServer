953b59802b9e0ff0a196aab22c5a1d89b09a0fd9
Ewan <vulcanv6@hotmail.com>
2017-08-18 23:27:23 +0100

    Update giveaway.py
    added missing import of commands from ext and fixed indentation error

--- giveaway.py
+++ giveaway.py
@@ -1,4 +1,5 @@
 import discord
+from discord.ext import commands
 
 class Giveaway:
     """General Dev Bot Commands"""
@@ -6,12 +7,13 @@ class Giveaway:
     def __init__(self, bot):
         self.bot = bot
         
-        @commands.command(pass_context=True)
-        async def giveaway(self,em):
+    @commands.command(pass_context=True)
+    async def giveaway(self,em):
     	"""Enter The Gift Card Giveaway!"""
-            em = discord.Embed(description='YOU HAVE BEEN ENTERED INTO THE GIFTCARD GIVEAWAY!', colour=0xFF0000)
-   	        em.set_author(name='GIVEAWAY BOT', url='discord.me/gdl')
-   	        em.set_thumbnail(url='http://isahotel.com.au/wp-content/uploads/2016/02/Gift_Card.jpg')
+    	em = discord.Embed(description='YOU HAVE BEEN ENTERED INTO THE GIFTCARD GIVEAWAY!', colour=0xFF0000)
+    	em.set_author(name='GIVEAWAY BOT', url='discord.me/gdl')
+    	em.set_thumbnail(url='http://isahotel.com.au/wp-content/uploads/2016/02/Gift_Card.jpg')
+    	await self.bot.say(embed=em) 
 
 def setup(bot):
     bot.add_cog(Giveaway(bot))
