51359ab14daab15b443bb96f77f7b24be9ae6d79
Ewan <vulcanv6@hotmail.com>
2017-08-18 23:30:52 +0100

    Update giveaway.py
    fixed bad https request

--- giveaway.py
+++ giveaway.py
@@ -11,7 +11,7 @@ class Giveaway:
     async def giveaway(self,em):
     	"""Enter The Gift Card Giveaway!"""
     	em = discord.Embed(description='YOU HAVE BEEN ENTERED INTO THE GIFTCARD GIVEAWAY!', colour=0xFF0000)
-    	em.set_author(name='GIVEAWAY BOT', url='discord.me/gdl')
+    	em.set_author(name='GIVEAWAY BOT', url='https://discord.me/gdl')
     	em.set_thumbnail(url='http://isahotel.com.au/wp-content/uploads/2016/02/Gift_Card.jpg')
     	await self.bot.say(embed=em) 
 
