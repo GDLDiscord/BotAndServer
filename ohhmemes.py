21f8d6c80f451d1afd6cdf230029c3de3e96c79e
Ewan <vulcanv6@hotmail.com>
2017-08-30 11:43:41 +0100

    Update ohhmemes.py
    needs memes

--- ohhmemes.py
+++ ohhmemes.py
@@ -1,1 +1,23 @@
+import discord
+from discord.ext import commands
+import random
 
+client = discord.Client()
+
+class OHHMemes:
+    """Display a random meme from the "Our Humber Home" server """
+
+    def __init__(self, bot):
+        self.bot = bot
+
+
+    @commands.command()
+    async def ohhmeme(self):
+
+        memes = [
+            "**OHH Meme #1:** ",
+    ]
+        choice = random.choice(memes)
+        await self.bot.say(choice)
+def setup(bot):
+    bot.add_cog(OHHMemes(bot))
